0c764aeb7df7f914e9a0012dbd8aad61879d1c49
Stacie Clere-Enoka <stacie@love.com>
2016-07-11 15:07:16 +1000

    Update get request variables.

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function search(obj,e){
 			part:'snippet,id',
 			q: q,
 			type:'video',
-			key:'AIzaSyAaPtipSvH1JsJgvi6aAcNFP00YCjehasQ'},
+			key:'SUPER-SECRET-YT-API-KEY'},
 			
 			function(data) {
 				var nextPageToken = data.nextPageToken;
